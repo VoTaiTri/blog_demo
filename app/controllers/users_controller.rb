538438d21f532cd46131dcb79f9e302f63a9b24a
VoTaiTri <vo.tai.tri@framgia.com>
2015-01-28 15:35:57 +0700

    Completed users-action: create, edit, update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,15 @@
 class UsersController < ApplicationController
+	before_action :logged_in_user, only: [:index, :edit, :update]
+	before_action :correct_user, 	only: [:edit, :update, :destroy]
+	before_action :admin_user,     	only: :destroy
+
+
 	def new
 		@user = User.new
+  	end
+
+    def index
+  		@users = User.paginate(page: params[:page])
   	end
 
   	def show
@@ -19,6 +28,20 @@ class UsersController < ApplicationController
 	    end
   	end
 
+  	def edit
+    	@user = User.find(params[:id])
+  	end
+
+  	def update
+	    @user = User.find(params[:id])
+	    if @user.update_attributes(user_params)
+	      	flash[:success] = "Profile Updated"
+	      	redirect_to @user
+	    else
+      		render 'edit'
+    	end
+  	end
+
 
   	private
 
@@ -26,4 +49,22 @@ class UsersController < ApplicationController
 	      	params.require(:user).permit(:name, :email, :password,
 	                                   :password_confirmation)
 	    end
+
+	    # Before filters
+
+	    # Confirms a logged-in user.
+	    def logged_in_user
+	      unless logged_in?
+	      	store_location
+	        flash[:danger] = "Please log in."
+	        redirect_to login_url
+	      end
+	    end
+
+	    # Confirms the correct user.
+    	def correct_user
+      		@user = User.find(params[:id])
+      		redirect_to(root_url) unless current_user?(@user)
+    	end
+    	
 end
